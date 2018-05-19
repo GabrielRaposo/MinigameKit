7659360518a41370be495d2e5dbdd3ce3796c313
GabrielRaposo <gabriel.s.raposo@gmail.com>
2018-05-19 12:54:44 -0300

    Preparo de terreno
    Fluxo de telas básico e Text Mesh Pro adicionados ao projeto

---
+++ MinigameKit/Assets/SceneLoadManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoadManager : MonoBehaviour {
+
+	public void LoadScene(string sceneName) {
+        SceneManager.LoadScene(sceneName);
+        //AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneName);
+        //asyncOperation.allowSceneActivation = false;
+    }
+}
