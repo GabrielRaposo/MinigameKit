11816b7fde88d21e8ae62320b2fbd054f0f6517b
juliorkm <julioramakrsnamandoju@gmail.com>
2018-05-20 19:22:59 -0300

    Começo das variáveis/funções estáticas de jogador e medley
    - Criada classe PlayerManager que controla quem são os jogadores
    atualmente no minigame, o resultado do minigame e as cores de cada
    jogador;
    - Escolhidas cores não-finais para os 4 primeiros jogadores;
    - Criada classe MedleyManager que controla e encapsula tudo que diz
    respeito ao medley;
    - Criado sistema de matchups que gera todos os matchups para um número
    de jogadores (sem repetição) e os aleatoriza numa pilha;
    - Criada função que gera matchups para um grupo seleto de jogadores
    (para desempate);
    - Criada função que encapsula a atualização de pontos dos jogadores;
    - Colocados sprites placeholder na tela título para testar as cores;
    - TMPro aparentemente atualizado (deu erro de compilação aqui,
    atualizei, funcionou).

---
+++ MinigameKit/Assets/Scripts/MedleyManager.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class MedleyManager : MonoBehaviour {
+
+    struct matchup {
+        public int leftPlayer;
+        public int rightPlayer;
+
+        public matchup (int left, int right) {
+            leftPlayer = left;
+            rightPlayer = right;
+        }
+
+        public override string ToString() {
+            return leftPlayer + " vs " + rightPlayer;
+        }
+    }
+
+    public static readonly int maxNumberOfPlayers = 4;
+
+    public static int maxScore;
+    public static int numberOfPlayers;
+    public static int[] playerScore;
+    private static Stack<matchup> matchupStack;
+    
+    void ResetScore() {
+        playerScore = new int[numberOfPlayers];
+        for (int i=0; i<numberOfPlayers; i++)
+            playerScore[i] = 0;
+    }
+
+    /// <summary>
+    /// Gera os matchups totais entre todos os jogadores, sem duplicatas.
+    /// </summary>
+    public static void GenerateMatchups() {
+        matchupStack = new Stack<matchup>();
+        for (int i = 0; i < numberOfPlayers; i++) {
+            for (int j = i+1; j < numberOfPlayers; j++) {
+                matchupStack.Push(new matchup(i, j));
+            }
+        }
+
+        // Shuffle
+        matchupStack = new Stack<matchup>(matchupStack.OrderBy(x => Random.Range(-100, 100)));
+    }
+
+    /// <summary>
+    /// Gera os matchups entre os jogadores que porventura empataram ao termino dos matchups originais.
+    /// </summary>
+    /// <param name="tiedPlayers">Array de inteiros identificadores de cada jogador (comecando em 0)</param>
+    public static void GenerateMatchups(int[] tiedPlayers) {
+        matchupStack = new Stack<matchup>();
+        for (int i = 0; i < tiedPlayers.Length; i++) {
+            for (int j = i + 1; j < tiedPlayers.Length; j++) {
+                matchupStack.Push(new matchup(tiedPlayers[i], tiedPlayers[j]));
+            }
+        }
+
+        // Shuffle
+        matchupStack = new Stack<matchup>(matchupStack.OrderBy(x => Random.Range(-100, 100)));
+    }
+
+    /// <summary>
+    /// Pega o proximo matchup da pilha gerada e coloca os respectivos jogadores nas variaveis estaticas correspondentes.
+    /// </summary>
+    public static void PickNextMatchup() {
+        matchup m = matchupStack.Pop();
+        PlayersManager.currentLeftPlayer = m.leftPlayer;
+        PlayersManager.currentRightPlayer = m.rightPlayer;
+    }
+
+    /// <summary>
+    /// Funcao de teste para conferir a aleatoriedade do embaralhamento de matchups.
+    /// </summary>
+    private void ShuffleTest() {
+        List<int> l = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        foreach (int i in l.ToArray())
+            print(i);
+        l = new List<int>(l.OrderBy(x => Random.Range(-100, 100)));
+        print("-----");
+        foreach (int i in l.ToArray())
+            print(i);
+    }
+
+    /// <summary>
+    /// Chamar depois de concluido um minigame. Encapsula a atualizacao de pontos dos envolvidos.
+    /// </summary>
+    private void UpdateScore() {
+        if (PlayersManager.result == 1) {
+            playerScore[PlayersManager.currentLeftPlayer]++;
+        }
+        else if (PlayersManager.result == 2) {
+            playerScore[PlayersManager.currentRightPlayer]++;
+        }
+        else {
+            // Caso empate de ponto para ambos:
+            // playerScore[PlayersManager.currentLeftPlayer]++;
+            // playerScore[PlayersManager.currentRightPlayer]++;
+        }
+    }
+}
---
+++ MinigameKit/Assets/Scripts/PlayersManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayersManager : MonoBehaviour {
+
+    // Cores escolhidas por Julio num color picker (nao sao finais)
+    public static Color[] playerColor = new Color[] {
+            new Color(.957f, .259f, .259f),    // Vermelho
+            new Color(.259f, .678f, .957f),    // Azul Claro
+            new Color(.957f, .922f, .259f),    // Amarelo
+            new Color(.137f, .627f, .184f)     // Verde Escuro
+        };
+
+    public static int currentLeftPlayer;
+    public static int currentRightPlayer;
+    /// <summary>
+    /// 0 = no results yet
+    /// 1 = Left Player Victory
+    /// 2 = Right Player Victory
+    /// 3 = DRAW
+    /// </summary>
+    public static int result;
+}
