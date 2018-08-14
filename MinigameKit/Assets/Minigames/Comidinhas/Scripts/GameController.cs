49443cc1ea78b362b69b2963de560b57d4d9f3b7
Joao Rabello <rabellois@hotmail.com>
2018-08-13 23:01:45 -0300

    Criação do Timer e Cores
    -Adicionado timer de preparação (3, 2, 1, GO!) e um timer para o minigame todo (30 segundos)
    -Inseridas as modificações de cores para os jogadores da esquerda e direita
    -Font Asset criado

---
+++ MinigameKit/Assets/Minigames/Comidinhas/Scripts/GameController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace Comidinhas
+{
+    public class GameController : MonoBehaviour {
+
+        public bool startTimer = false;
+        public float readyTime = 3f;
+        public float time = 30f;
+        public TextMeshProUGUI timerText;
+        public TextMeshProUGUI readyTimerText;
+
+	    void Start () {
+
+            timerText.text = null;
+
+            StartCoroutine(ReadyTimer());
+
+	    }
+	
+	    
+	    void Update () {
+
+            if (startTimer)
+            {
+                time -= Time.deltaTime;
+                timerText.text = time.ToString("0");
+            }
+            else
+            {
+                readyTime -= Time.deltaTime;
+                if (readyTime >= 0.5f) readyTimerText.text = readyTime.ToString("0");
+                else readyTimerText.text = "GO";
+            }
+
+	    }
+
+        IEnumerator ReadyTimer()
+        {
+           
+            yield return new WaitForSeconds(3);
+            startTimer = true;
+            readyTimerText.enabled = false;
+            StopCoroutine(ReadyTimer());
+        }
+    }
+}
+
---
+++ MinigameKit/Assets/Minigames/Comidinhas/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Comidinhas
+{
+    public class Player : PlayerInfo {
+
+	    
+
+
+	    public override void Start() {
+
+            base.Start();
+
+            GetComponent<SpriteRenderer>().color = base.color;
+	    }
+	
+	    
+	    void Update () {
+		    
+	    }
+
+        private void OnCollisionEnter(Collision col)
+        {
+            if (col.gameObject.CompareTag("Comida"))
+            {
+                Destroy(col.gameObject);
+                
+            }
+        }
+    }
+}
