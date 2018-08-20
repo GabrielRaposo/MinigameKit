29b5cd9300e16e61668e18e66049398a6b033bf6
Igor <igorsc.jerry@gmail.com>
2018-08-20 20:05:24 -0300

    Comit Inicial

---
+++ MinigameKit/Assets/Minigames/Bailarinas/Scripts/BailarinaScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Bailarinas
+{
+
+    public class BailarinaScript : PlayerInfo
+    {
+  
+        private Rigidbody rb;
+
+        // Use this for initialization
+        override public void Start()
+        {
+            base.Start();
+            rb = GetComponent<Rigidbody>();
+            
+        }
+                
+        void Update()
+        {
+            if (Input.GetButtonDown(playerButtons.action))
+            {
+                rb.AddForce(new Vector3(0, 0, 300.0f));
+            }
+
+            if (Input.GetAxisRaw(playerButtons.vertical) >= 0.5f)
+            {
+                rb.AddForce(new Vector3(0, 0, 3.0f));
+            }
+
+        }
+    }
+
+}
+
+
