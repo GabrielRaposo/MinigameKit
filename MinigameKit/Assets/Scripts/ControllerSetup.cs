bc1fff9b8439adf470b7345c028c9f905d1aa3c8
GabrielRaposo <gabriel.s.raposo@gmail.com>
2018-08-22 22:19:23 -0300

    Tela de setup de controles modificada

--- MinigameKit/Assets/Scripts/ControllerSetup.cs
+++ MinigameKit/Assets/Scripts/ControllerSetup.cs
@@ -2,29 +2,94 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using TMPro;
 
 public class ControllerSetup : MonoBehaviour {
 
-    public TextMeshProUGUI leftPlayer, rightPlayer;
+    [Header("Indicators")]
+    public ControllerSetupIconManager CSleft;
+    public ControllerSetupIconManager CSright;
+
+    [Header("Finish")]
+    public Image diamondShape;
     public Button returnButton;
+    public TextMeshProUGUI buttonTextOutline;
+    public TextMeshProUGUI buttonTextFill;
+
+    int state; //0 - left, 1 - right, 2 - finish
 
     void OnEnable () {
         ControllerManager.instance.ResetSetup();
-        leftPlayer.text = "";
-        rightPlayer.text = "";
-        if(returnButton) returnButton.interactable = false;
+        CSleft.ResetIcons();
+        CSright.ResetIcons();
+        if (returnButton) returnButton.interactable = false;
+        SetState(0);
     }
 	
 	void Update () {
-        if (ControllerManager.instance != null) {
-            if (leftPlayer.text == "" || rightPlayer.text == "") {
-                ControllerManager.instance.ControllerSetup();
-                leftPlayer.text = ControllerManager.instance.GetLeftController();
-                rightPlayer.text = ControllerManager.instance.GetRightController();
-            } else {
-                if (returnButton) returnButton.interactable = true;
+        string input;
+        if (ControllerManager.instance != null)
+        {
+            switch (state)
+            {
+                case 0:
+                    ControllerManager.instance.ControllerSetup();
+                    input = ControllerManager.instance.GetLeftController();
+                    if(input != string.Empty)
+                    {
+                        CSleft.SetControllerIcon(input);
+                        SetState(1);
+                    }
+                    break;
+
+                case 1:
+                    ControllerManager.instance.ControllerSetup();
+                    input = ControllerManager.instance.GetRightController();
+                    if (input != string.Empty)
+                    {
+                        CSright.SetControllerIcon(input);
+                        SetState(2);
+                    }
+                    break;
+
+                default:
+                    if (returnButton)
+                    {
+                        returnButton.interactable = true;
+                        EventSystem.current.SetSelectedGameObject(returnButton.gameObject);
+                    }
+                    break;
             }
+        }
+    }
+
+    void SetState(int value)
+    {
+        state = value;
+
+        switch (state)
+        {
+            case 0:
+                CSleft.SetSelection(true);
+                CSright.SetSelection(false);
+                diamondShape.enabled = false;
+                buttonTextOutline.enabled = true;
+                buttonTextFill.enabled = false;
+                break;
+
+            case 1:
+                CSleft.SetSelection(false);
+                CSright.SetSelection(true);
+                break;
+
+            default:
+                CSleft.SetSelection(false);
+                CSright.SetSelection(false);
+                diamondShape.enabled = true;
+                buttonTextOutline.enabled = false;
+                buttonTextFill.enabled = true;
+                break;
         }
     }
 }
---
+++ MinigameKit/Assets/Scripts/ControllerSetupIconManager.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControllerSetupIconManager : MonoBehaviour {
+    const float ORIGINAL_Y = 60f;
+    const float ORIGINAL_SCALE = .37f;
+    const float BIG_SCALE = .55f;
+
+    [Header("UI References")]
+    public Image keyboardOutline;
+    public Image keyboardFill;
+
+    public Image joystickOutline;
+    public Image joystickFill;
+
+    public GameObject inputFlashing;
+
+    public Image diamondShape;
+
+    enum Controller { None, Keyboard, Joystick }
+    Controller currentController;
+
+    RectTransform keyboardTransform;
+    RectTransform joystickTransform;
+
+    private void Awake()
+    {
+        keyboardTransform = keyboardOutline.GetComponent<RectTransform>();
+        joystickTransform = joystickOutline.GetComponent<RectTransform>();
+    }
+	
+	public void ResetIcons()
+    {
+        StopAllCoroutines();
+
+        keyboardOutline.fillAmount = 1;
+        keyboardFill.fillAmount = 0;
+
+        joystickOutline.enabled = true;
+        joystickFill.enabled = false;
+
+        keyboardTransform = keyboardOutline.GetComponent<RectTransform>();
+        joystickTransform = joystickOutline.GetComponent<RectTransform>();
+        keyboardTransform.localScale = Vector2.one * ORIGINAL_SCALE;
+        joystickTransform.localScale = Vector2.one * ORIGINAL_SCALE;
+
+        keyboardTransform.anchoredPosition = Vector2.up   * ORIGINAL_Y;
+        joystickTransform.anchoredPosition = Vector2.down * ORIGINAL_Y;
+
+        inputFlashing.SetActive(false);
+        currentController = Controller.None;
+    }
+
+    public void SetSelection(bool value)
+    {
+        diamondShape.enabled = value;
+        inputFlashing.SetActive(value);
+    }
+
+    public void SetControllerIcon(string value)
+    {
+        if(value[0] == 'K') {
+            SetKeyboard(value);
+        } else {
+            SetJoystick();
+        }
+        inputFlashing.SetActive(false);
+    }
+
+    void SetKeyboard(string value)
+    {
+        StartCoroutine(FocusIcon(keyboardTransform, joystickTransform));
+        keyboardFill.enabled = true;
+
+        if (value[1] == '1') {
+            keyboardFill.fillOrigin = 0;
+            keyboardOutline.fillOrigin = 1;
+            keyboardFill.fillAmount = .5f;
+            keyboardOutline.fillAmount = .5f;
+        } else {
+            keyboardFill.fillOrigin = 1;
+            keyboardOutline.fillOrigin = 0;
+            keyboardFill.fillAmount = .5f;
+            keyboardOutline.fillAmount = .5f;
+        }
+
+        joystickOutline.enabled = false;
+    }
+
+    void SetJoystick()
+    {
+        StartCoroutine(FocusIcon(joystickTransform, keyboardTransform));
+        joystickOutline.enabled = false;
+        keyboardOutline.fillAmount = 0;
+        keyboardFill.enabled = false;
+        joystickFill.enabled = true;
+    }
+
+    IEnumerator FocusIcon(RectTransform iconHighlight, RectTransform iconHide)
+    {
+        Vector3 moveDirection = (iconHighlight.localPosition.y > 0) ? Vector2.down : Vector2.up;
+        float speed = 10f;
+        float numOfSteps = ORIGINAL_Y / speed;
+        float growthRate = (BIG_SCALE - ORIGINAL_SCALE) / numOfSteps;
+        float hideRate = ORIGINAL_SCALE / numOfSteps;
+
+        while (Mathf.Abs(iconHighlight.localPosition.y) > 0)
+        {
+            iconHighlight.localPosition += moveDirection * speed;
+            iconHighlight.localScale += Vector3.one * growthRate;
+            iconHide.localScale -= Vector3.one * hideRate;
+            yield return new WaitForEndOfFrame();
+        }
+
+        iconHighlight.localPosition = Vector3.zero;
+        iconHighlight.localScale = Vector3.one * BIG_SCALE;
+        iconHide.localScale = Vector3.zero;
+    }
+}
