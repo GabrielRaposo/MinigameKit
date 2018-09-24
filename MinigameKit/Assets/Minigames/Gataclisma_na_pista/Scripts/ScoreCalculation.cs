95cd06fcb41da70b67ba601a9a7378eb14c22861
Eduardo Melo <eduardodiasmelo@gmail.com>
2018-09-23 23:40:31 -0300

    Crown

--- MinigameKit/Assets/Minigames/Gataclisma_na_pista/Scripts/ScoreCalculation.cs
+++ MinigameKit/Assets/Minigames/Gataclisma_na_pista/Scripts/ScoreCalculation.cs
@@ -8,9 +8,11 @@ using DG.Tweening;
 namespace GataclismaNaPista
 {
     public class ScoreCalculation : MonoBehaviour {
-        
+
         /*TODO: update do score com evento OnScoreChange()*/
 
+        public GameObject crown;
+        private Vector2 crownPosition;
         private Slider slider;
         private float scoreLeft;
         private float scoreRight;
@@ -22,11 +24,25 @@ namespace GataclismaNaPista
             players = GameObject.FindObjectsOfType<PlayerController>();
         }
 
+        private void Start()
+        {
+            crownPosition = crown.transform.position;
+        }
+
         public void UpdateScorebar()
         {
             scoreLeft = players[0].Score;
             scoreRight = players[1].Score;
             slider.DOValue(slider.maxValue * scoreLeft / (scoreLeft + scoreRight), 0.2f);
+
+            if (players[0].Score > players[1].Score)
+            {
+                crown.transform.position = crownPosition;
+            }
+            else if (players[1].Score > players[0].Score)
+            {
+                crown.transform.position = crownPosition * new Vector2(-1, 1);
+            }
         }
     }
 }
